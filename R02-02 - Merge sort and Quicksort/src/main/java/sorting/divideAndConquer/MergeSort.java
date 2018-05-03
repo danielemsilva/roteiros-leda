a90eb1912395ab5315f1ded57686b3f9c86edd42
danielemsilva <daniele.silva@ccc.ufcg.edu.br>
2018-05-03 20:22:31 -0300

    Added comments

--- R02-02 - Merge sort and Quicksort/src/main/java/sorting/divideAndConquer/MergeSort.java
+++ R02-02 - Merge sort and Quicksort/src/main/java/sorting/divideAndConquer/MergeSort.java
@@ -13,26 +13,33 @@ import sorting.AbstractSorting;
 public class MergeSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
 	private void merge(T[] array, int leftIndex, int middle, int rightIndex) {
+		// Creates an array equal to the original
 		T[] auxArray = Arrays.copyOf(array, array.length);
+		// This index is the beginning of the left array
 		int i = leftIndex;
+		// This index is the beginning of the right array
 		int j = middle + 1;
+		// This index will go through the array
 		int k = leftIndex;
+		// Here goes through the two arrays looking for the lowest value
 		while (i <= middle && j <= rightIndex) {
-			if (auxArray[i].compareTo(auxArray[j]) < 0) {
+			// The lowest is in the left array
+			if (auxArray[i].compareTo(auxArray[j]) <= 0) {
 				array[k] = auxArray[i];
-				i++;
+				i++; // Increments the index to the next position of this array
 			} else {
 				array[k] = auxArray[j];
 				j++;
 			}
 			k++;
 		}
-		
+		// Add the rest of the left array, if there is any
 		while (i <= middle) {
 			array[k] = auxArray[i];
 			i++;
 			k++;
 		}
+		// Add the rest of the right array, if there is any
 		while (j <= rightIndex) {
 			array[k] = auxArray[j];
 			j++;
@@ -42,12 +49,15 @@ public class MergeSort<T extends Comparable<T>> extends AbstractSorting<T> {
 	
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
+		// leftIndex should not exceed the rightIndex
 		if (leftIndex < rightIndex) {
+			// Finds the middle position
 			int middle = (leftIndex + rightIndex) / 2;
-			System.out.println(middle);
+			// Separates the first half of the array
 			sort(array, leftIndex, middle);
+			// Separates the second half of the array
 			sort(array, middle + 1, rightIndex);
-			
+			// Merges parts of the array to sort
 			merge(array, leftIndex, middle, rightIndex);
 		}
 	}
