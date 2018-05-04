a4c4f36f17c03db4a1aea0752c57dcbf773e8306
danielemsilva <daniele.silva@ccc.ufcg.edu.br>
2018-05-03 22:11:43 -0300

    Adding quick sort

--- R02-02 - Merge sort and Quicksort/src/main/java/sorting/divideAndConquer/QuickSort.java
+++ R02-02 - Merge sort and Quicksort/src/main/java/sorting/divideAndConquer/QuickSort.java
@@ -1,6 +1,7 @@
 package sorting.divideAndConquer;
 
 import sorting.AbstractSorting;
+import util.Util;
 
 /**
  * Quicksort is based on the divide-and-conquer paradigm. The algorithm chooses
@@ -12,9 +13,32 @@ import sorting.AbstractSorting;
  */
 public class QuickSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
-	@Override
-	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
-	}
+   private int separate(T[] array, int leftIndex, int rightIndex) {
+      // Assumes that the smallest element is in the last position
+      T pivot = array[rightIndex];
+
+      int j = leftIndex - 1;
+      // Here goes through the array looking for the smallest 
+      // and switching to the first positions
+      for (int i = leftIndex; i < rightIndex; i++) {
+         if (array[i].compareTo(pivot) <= 0) {
+            j++;
+            Util.swap(array, i, j);
+         }
+      }
+      Util.swap(array, j + 1, rightIndex);
+      // This index indicates the number that is in the middle
+      return j + 1;
+   }
+
+   @Override
+   public void sort(T[] array, int leftIndex, int rightIndex) {
+      if (leftIndex < rightIndex) {
+         int particion = separate(array, leftIndex, rightIndex);
+         // Here is the first part of the array, with values smaller than the pivot
+         sort(array, leftIndex, particion - 1);
+         // In this part are the values greater than the pivot
+         sort(array, particion + 1, rightIndex);
+      }
+   }
 }
